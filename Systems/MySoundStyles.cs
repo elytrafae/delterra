7e468b6fbfa4a2a34182864f9e3f32922e657712
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-07-11 22:15:35 +0300

    Added Sound Style system

---
+++ Systems/MySoundStyles.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+
+namespace YetToBeNamed.Systems {
+    public class MySoundStyles {
+
+        // To anyone who wants to help me add sound effects,
+        // all you have to do is copy the following line and change "SoundNameInCode" and 
+        // "sound_filename" for the name of the sound file inside the SoundEffects folder (without .ogg!)
+        // public static readonly SoundStyle SoundNameInCode = Register("sound_filename"); 
+
+
+
+        private static SoundStyle Register(string name) { 
+            return new SoundStyle(nameof(YetToBeNamed) + "/Assets/SoundEffects/" + name);
+        }
+
+    }
+}
