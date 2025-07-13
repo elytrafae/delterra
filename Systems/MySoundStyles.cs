39e81a7c35f3f19aba4bf94b0a4217ba4aad8327
elytrafae <48065803+elytrafae@users.noreply.github.com>
2025-07-13 20:37:48 +0300

    More vanity code, moved frostmancer sprites to a separate folder

--- Systems/MySoundStyles.cs
+++ Systems/MySoundStyles.cs
@@ -22,7 +22,7 @@ namespace Delterra.Systems {
         public static readonly SoundStyle Petrify = RegisterSound("Petrify");
         public static readonly SoundStyle RudeBusterHit = RegisterSound("Rudebuster_Hit");
         public static readonly SoundStyle RudeBusterSwing = RegisterSound("Rudebuster_Swing");
-        public static readonly SoundStyle SnowgraveBell = RegisterSound("Snowgrave_Bell");
+        public static readonly SoundStyle SnowgraveBell = RegisterSound("Snowgrave_Bell").WithVolumeScale(0.9f);
         public static readonly SoundStyle SnowgraveCast = RegisterSound("Snowgrave_Cast").WithVolumeScale(1.3f);
         public static readonly SoundStyle Tension = RegisterSound("Tension");
 
